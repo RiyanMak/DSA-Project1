268df8f16e0216a5f4d120db2967896be71715f1
Riyan Maknojia <riyanmak123@gmail.com>
2025-03-03 17:55:53 -0600

    created node class and intialize head to null

--- IDedLinkedList.java
+++ IDedLinkedList.java
@@ -1,6 +1,24 @@
 class IDedLinkedList <AnyType extends IDedObject>{
 
-    public IDedLinkedList (){
-        
+    private static class Node<AnyType>{ //Created private class to implement Node for anytype 
+        AnyType data; //The data
+        Node<AnyType> next; //Refrence to the next node
+
+        public Node(AnyType data){ //Constructor that intialize head and the next node refrence value
+            this.data = data;  
+            this.next = null;
+        }  
+        public Node(AnyType data, Node<AnyType> next){
+            this.data = data;
+            this.next = next;
+        }
     }
+    
+    private Node<AnyType> head  = null;
+
+
+    public IDedLinkedList(){
+        //Default linked list since head is empty
+    }
+
 }
